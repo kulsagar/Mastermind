9aeaf9465be3e065add5c50486b4f3c8f27f1190
Kulkarni <sagar.a.kulkarni@capgemini.com>
2019-09-02 13:26:49 +0530

    drop completed for all rows

--- dragNdrop.js
+++ dragNdrop.js
@@ -1,17 +1,18 @@
 function drag(ev) {
     ev.dataTransfer.setData("id", ev.target.id);
+    console.log("id:: ",ev.target.id);
   }
 
   function allowDrop(ev) {
     ev.preventDefault();
   }
 
-  function drop(ev) {
+  function drop(ev,id) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("id");
-    console.log(data);
+    console.log(data,id);
     document.getElementById(data).cloneNode(true)
     
     
-    document.getElementById(ev.target.id).innerHTML = "<div  id='one' class='"+"small"+data+"'></div>"
+    document.getElementById(ev.target.id).innerHTML = "<div  id='"+id+"' class='"+"small"+data+"'></div>"
   }
