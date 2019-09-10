339bb5d29e9b7504a6ba303b76fd80dec14300c5
Kulkarni <sagar.a.kulkarni@capgemini.com>
2019-09-10 21:43:25 +0530

    completed drag and drop

--- dragNdrop.js
+++ dragNdrop.js
@@ -1,10 +1,16 @@
 let rowColors = [
-  "white","blue","pink","black"
+  "white","white","white","white"
 ];
-function colorReplacer(data){
+
+let cols = ["one", "two", "three", "four"];
+
+function colorReplacer(data, row){
   for (i = 0, len = rowColors.length; i < len; i++){
     if(data===rowColors[i]){
       rowColors[i]="white";
+      let id = row+"_"+cols[i];
+      //console.log(id);
+      document.getElementById(id).innerHTML = "<div  id='"+id+"' class='white'></div>"
       break;
     }
   }
@@ -21,30 +27,31 @@ function drag(ev) {
   function drop(ev,id) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("id");
-    console.log(data,id,);
+    //console.log(data,id,);
     document.getElementById(data).cloneNode(true)
     
    let position = id.split("_");
   // console.log(position[1]);
    switch (position[1]){
      case "one":
-      colorReplacer(data);
+      colorReplacer(data,position[0]);
       rowColors[0]=data;
        break;
     case "two":
-        colorReplacer(data);
+        colorReplacer(data, position[0]);
         rowColors[1]=data;
       break;
     case "three":
-        colorReplacer(data);
+        colorReplacer(data, position[0]);
         rowColors[2]=data;
         break;
     case "four":
-        colorReplacer(data);
+        colorReplacer(data, position[0]);
         rowColors[3]=data;
         break;
     default:
            console.log("else");
    } 
+   //console.log(rowColors);
     document.getElementById(ev.target.id).innerHTML = "<div  id='"+id+"' class='"+"small"+data+"'></div>"
   }
